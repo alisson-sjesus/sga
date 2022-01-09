b7d1c0f10aa3f3195f83f8042987c7ca533eccbd
Alisson Santos <alissoon.santos@gmail.com>
2022-01-08 23:31:27 -0300

    funcao de limpar nfce

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const btnCopiar = document.querySelector('.btn-copiar');
 const btnGerar = document.querySelector('.btn-nfce');
 
+btnGerar.addEventListener('click', () => {
+  let inputNFCE = document.querySelector('.input-nfce');
+  inputNFCE = inputNFCE.value;
+
+  limpar(inputNFCE);
+
+  btnGerar.innerText = 'Gerado ✔';
+  btnGerar.style.width = '120px';
+  setTimeout(() => {
+    btnGerar.style.width = '90px';
+    btnGerar.innerText = 'Gerar';
+  }, 2000);
+});
+
 btnCopiar.addEventListener('click', () => {
   btnCopiar.innerText = 'Copiado ✔';
   btnCopiar.style.width = '120px';
@@ -10,11 +24,18 @@ btnCopiar.addEventListener('click', () => {
   }, 2000);
 });
 
-btnGerar.addEventListener('click', () => {
-  btnGerar.innerText = 'Gerado ✔';
-  btnGerar.style.width = '120px';
-  setTimeout(() => {
-    btnGerar.style.width = '90px';
-    btnGerar.innerText = 'Gerar';
-  }, 2000);
-});
+function limpar(nfce) {
+  const outputNFCE = document.querySelector('.output-nfce');
+  const regex = /(?:0{2,})(\d+)(?:\s\d+)?/gm;
+  let regexpResult;
+
+  while ((regexpResult = regex.exec(nfce)) !== null) {
+    outputNFCE.innerHTML += regexpResult[1] + ',';
+  }
+
+  const resultadoArray = outputNFCE.innerHTML
+    .split(',')
+    .filter((arr) => arr)
+    .join(',');
+  outputNFCE.innerHTML = resultadoArray;
+}
